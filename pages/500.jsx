dd5888180330e69b5d47a619fb454f3f59274bdd
tc0dev <104026747+tc0dev@users.noreply.github.com>
2024-05-10 17:10:54 +0300

    Update 500.jsx

--- pages/500.jsx
+++ pages/500.jsx
@@ -1,6 +1,6 @@
 import ErrorPage from '../components/ErrorPage';
 const UnkownPage = () => {
-    return <ErrorPage code={500} message={"Oh. It seems there is a 500 Internal Server Error. Please try again later!"} />
+    return <ErrorPage code={500} message={"Oh. Bu bir server hatası gibi görünüyor..."} />
 }
 
 export default UnkownPage;
